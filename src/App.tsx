675cae872635d4a6d3df8c01b63c8491e146e493
Gui089 <guilhermesilba811@gmail.com>
2024-07-16 10:52:19 -0300

    feat: add promo banner component

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,27 @@ function App() {
        </div>
 
        <CategoryList />
+
+      <div className="bg-red-600 mt-5 w-[350px] h-[150px] ml-8 rounded-md flex justify-between">
+
+          <div className=" flex flex-col mt-3 items-center ml-3 h-[90px]">
+              <p className="text-white ">até</p>
+              <div className="flex justify-center items-center">
+              <h1 className="text-white font-bold text-3xl">30%</h1>
+              <p className="text-white" >de</p>
+              </div>
+              <h1 className="text-white font-bold text-3xl">Desconto</h1>
+              <p className="text-white">em pizzas</p>
+          </div>
+
+
+        <img 
+          className=""
+          width={180}
+          
+          src="/img/promo-banner-1.png" 
+          alt="Ate 30% de desconto em pizzas" />
+      </div>
     </>
   )
 }
